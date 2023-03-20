25ced699cb91243ae5d1d1b0d24611fbb2409efb
Yael tal <yaelital27@gmail.com>
2023-03-20 17:02:54 +0200

    trying more features

--- src/pages/Stars-page.jsx
+++ src/pages/Stars-page.jsx
@@ -1,8 +1,21 @@
 import { Stars } from "../cmps/stars";
+import { motion } from "framer-motion";
 
 export function StarsPage() {
 
     return <section style={{ height: 150 + 'vh' }} className="stars-page">
         <Stars />
+        <motion.h1
+                animate={{ x: [50, 150, 50], opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 5,
+                    delay: 0.3,
+                    ease: [0.5, 0.71, 1, 1.5],
+                }}
+                initial={{ opacity: 0, scale: 0.5 }}
+                whileHover={{ scale: 1.2 }}
+            >
+                Framer Motion
+            </motion.h1>
     </section>
 }
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,12 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { AnimatedSphere } from "../cmps/animated-sphere";
 import { AnimatedSphere1 } from "../cmps/bg-animated-sphere";
+import { Signup } from "./signup";
 
 export function HomePage() {
 
-    return <section className="home-page">
-        {/* <motion.div
+  const [start, setStart] = useState(false)
+
+  return <section className="home-page">
+    {/* <motion.div
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 1.1 }}
       drag="x"
@@ -15,23 +20,23 @@ export function HomePage() {
     >
       <h1 style={{ position: 'relative', zIndex: 5 }}>Quiska</h1>
     </motion.div> */}
-        {/* <Signup /> */}
-        <h1 style={{ position: 'relative', zIndex: 5 }}>Quiska</h1>
-        <Canvas className="canvas" style={{ height: 90 + 'vh', width: 50 + 'vw', position: 'absolute', zIndex: 1 }}>
-            <AnimatedSphere />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <OrbitControls enableZoom={false} />
-        </Canvas>
+    {/* <Signup /> */}
+  {  !start &&  <h1 style={{ position: 'relative', zIndex: 5 }}>Quiska</h1>}
+    <Canvas className="canvas" style={{ height: 90 + 'vh', width: 50 + 'vw', position: 'absolute', zIndex: 1 }}>
+      <AnimatedSphere />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-2, 5, 2]} intensity={1} />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
 
-        <Canvas className="canvas" style={{ height: 90 + 'vh', width: 60 + 'vw', position: 'absolute', zIndex: 0 }}>
-            <AnimatedSphere1 />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <OrbitControls enableZoom={false} />
-        </Canvas>
+    <Canvas className="canvas" style={{ height: 90 + 'vh', width: 60 + 'vw', position: 'absolute', zIndex: 0 }}>
+      <AnimatedSphere1 />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[-2, 5, 2]} intensity={1} />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
 
-        {/* <Canvas className="canvas" style={{ height: 90 + 'vh', width: 60 + 'vw', position: 'absolute', zIndex: 0 }}>
+    {/* <Canvas className="canvas" style={{ height: 90 + 'vh', width: 60 + 'vw', position: 'absolute', zIndex: 0 }}>
       <Suspense fallback={null}>
       <Box />
       </Suspense>
@@ -42,13 +47,13 @@ export function HomePage() {
 
     </Canvas> */}
 
-        {/* <Canvas className="canvas" style={{ height: 90 + 'vh', width: 90 + 'vw', position: 'absolute', zIndex: 0, top: 50, right: 500 + 'px' }}>
+    {/* <Canvas className="canvas" style={{ height: 90 + 'vh', width: 90 + 'vw', position: 'absolute', zIndex: 0, top: 50, right: 500 + 'px' }}>
       <AnimatedSphere2 />
       <ambientLight intensity={0.5} />
       <directionalLight position={[-2, 5, 2]} intensity={1} />
       <OrbitControls enableZoom={false} />
     </Canvas> */}
-
-        <button className='start-btn'>Start</button>
-    </section>
+    {start && <Signup />}
+    {!start && <button onClick={() => { setStart(!start) }} className='start-btn'>Start</button>}
+  </section>
 }
